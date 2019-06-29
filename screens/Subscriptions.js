7bc705e241bf6c8f61f409a02a1c75132dfff454
rpk79 <rajkharvar1808@gmail.com>
2019-06-29 12:40:20 +0530

    added channel logo in subscriptions screen

--- screens/Subscriptions.js
+++ screens/Subscriptions.js
@@ -1,11 +1,69 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, Image } from 'react-native';
+
+const images = [
+  {
+    id: 1,
+    source: require('../assets/raj.jpg')
+  },
+  {
+    id: 2,
+    source: require('../assets/nita.jpg')
+  },
+  {
+    id: 3,
+    source: require('../assets/pramod.jpg')
+  },
+  {
+    id: 4,
+    source: require('../assets/harshiv.jpg')
+  },
+  {
+    id: 5,
+    source: require('../assets/dharmistha.jpg')
+  }
+];
 
 class Subscriptions extends Component {
+  renderAllImages = () => {
+    return images.map(image => {
+      return (
+        <View style={{ flex: 1, marginLeft: 10 }} key={image.id}>
+          <Image
+            source={image.source}
+            style={{ width: 65, height: 65, borderRadius: 32.5 }}
+          />
+        </View>
+      );
+    });
+  };
   render() {
     return (
       <View style={styles.container}>
-        <Text>Subscriptions</Text>
+        <View
+          style={{
+            flexDirection: 'row',
+            borderBottomWidth: 1.5,
+            borderBottomColor: '#999',
+            marginVertical: 3
+          }}
+        >
+          <View
+            style={{
+              flex: 4,
+              alignItems: 'center'
+            }}
+          >
+            <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+              {this.renderAllImages()}
+            </ScrollView>
+          </View>
+          <View
+            style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
+          >
+            <Text style={{ fontSize: 18, color: '#0A79DF' }}>All</Text>
+          </View>
+        </View>
       </View>
     );
   }
@@ -15,7 +73,7 @@ export default Subscriptions;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
+    backgroundColor: '#333',
+    paddingTop: 30
   }
 });
